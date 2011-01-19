a6a72e6da76e91e469c9f1c912bfb6310f25a72a
Juan Carlos Quiroz <juan.carlos.quiroz.ramos@gmail.com>
2011-01-19 15:17:56 -0800

    Model Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110115010102) do
+ActiveRecord::Schema.define(:version => 20110117171939) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
   end
 
 end
